1ed96474b14f6d9d15b442a078e615256a266558
RodolfoB <rodosuna9@gmail.com>
2024-04-23 22:02:53 -0700

    Se agrega componente home para inicio

---
+++ src/Components/Home.js
@@ -0,0 +1,19 @@
+import React from "react"
+
+export const Home = () => {
+
+
+    return ( 
+        <>
+          <div className = "header" >
+          <div className = "hd" >
+          <img src = "./images/marvel.png"
+          alt = "" / >
+          </div> 
+          <div className="search-bar">
+            <input type="search" placeholder="Buscar.." className="search"></input>
+          </div>
+          </div> 
+          </>
+      )
+}
